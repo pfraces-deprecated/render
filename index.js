6a732fb616c48cba192b839f0dcab2d447783a4c
Pau Fracés <pfraces@gmail.com>
2013-02-15 22:22:57 +0100

    frame implementation

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-var sel = require('sel').plugin(require('sel-style'));
+var sel = require('sel');
+sel.plugin(
+  require('sel-style'),
+  require('sel-arrange')
+);
 
 module.exports = function (config) {
   return new Render(config);
 };
 
-var Render = function (config) {
-  sel('#' + config.id).
+var Render = function (config, actors) {
+  this.board = sel('#' + config.id).
     size({
       width: (config.width * config.cell).toString() + 'px',
       height: (config.height * config.cell).toString() + 'px'
     }).
     color({ bg: config.bg });
 
-  setInterval(config.frame, 1000 / config.fps);
+  var frame = function (actors) {
+    actors.forEach(function (actor) {
+      actor.members.forEach(function (member) {
+        member.el.pos({
+          x: ((actor.x + member.x) * config.cell).toString() + 'px',
+          y: ((actor.y + member.y) * config.cell).toString() + 'px'
+        })
+      });
+    });
+  };
+
+  setInterval(frame(actors), 1000 / config.fps);
 };
+
+Render.prototype.tile = (function () {
+  var self = this,
+      id = 0;
+
+  return function (color) {
+    return sel.div('tile' + id).
+      move({to: self.board});
+  };
+})();
